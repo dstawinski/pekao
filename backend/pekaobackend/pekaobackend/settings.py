9c81f2cd8f95ffa8dda9baf2bf41dd65f4c7309a
wkulikowski <wojtask98@gmail.com>
2019-02-23 17:34:59 +0100

    Sry im dumb

--- backend/pekaobackend/pekaobackend/settings.py
+++ backend/pekaobackend/pekaobackend/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'app.CorsMiddleware',
+    'eventify.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'pekaobackend.urls'
